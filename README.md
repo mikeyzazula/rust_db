247919cdf31c89d1d939c5b66f15ac48b4e12eec
mikey <mikeyzazula@gmail.com>
2021-08-11 21:33:26 -0700

    updated read me and fixed comments

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Reading in text files and using a Postgres DB with Rust
-
-
-
-
+Simple program that reads in a .txt file (provided in the src folder) then creates a Postgres database and places each line of text into the database.   
+By default it prints out the contents of the DB, and upon completion drops the table.   
+Ran on Ubuntu 20.04.2 LTS and OSX
 ## Running instructions:
+
+Running `cargo run` will load the dependencies and run the program if Postgres is installed
 
 ### Install Postgres via terminal:
 - `sudo apt update`
 - `sudo apt install postgresql postgresql-contrib`
-### Create postgres user:
+
+## Create Postgres user:
+    Alternate way to run the program, should not be necessary
 - `sudo -u postgres createuser --interactive`
 - "Enter name of role to add: 
 - `postgres`
@@ -18,5 +21,3 @@ Shall the new role be a superuser? (y/n)"
 - `sudo -i -u postgres`
 - `psql`
 - `ALTER USER postgres WITH PASSWORD 'myPassword';`
-### In a separate terminal run :
-`cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-//postgres crate to utilize basic db operations we'll need
+// Postgres crate to utilize basic db operations we'll need.
 use postgres::{Client, Error, NoTls};
 
-//fs is the module for basic filesystem manipulation, we just need File for opening
+// fs is the module for basic filesystem manipulation, we just need File for opening.
 use std::fs::File;
 
-//From io we need BufReader for it's additional line reading capabilities
-//TODO what the hell is prelude
-use std::io::{prelude::*, BufReader};
+// From io we need BufRead/er for it's additional line reading capabilities and functionality.
+use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Error> {
-    //Connecting to database
+    // Create a database called postgres if it does not exit, and then connect to it.
     let mut client = Client::connect(
         "postgresql://postgres:myPassword@localhost:5432/postgres",
         NoTls,
     )?;
 
-    //creating table for the DB
+    // Creating table for the DB.
     client.batch_execute(
         "CREATE TABLE IF NOT EXISTS rangers (
         id SERIAL PRIMARY KEY,
@@ -24,25 +23,27 @@ fn main() -> Result<(), Error> {
     ",
     )?;
 
-    //handling the io
+    // Handling the file io and setting the reader.
     let file = File::open("src/text_file.txt").expect("File open error");
     let reader = BufReader::new(file);
 
-    //inserting the text into the DB
+    // BufRead lets us insert the text into the DB line by line.
     for line in reader.lines() {
         client.execute("INSERT INTO rangers (name) VALUES ($1)", &[&line.ok()])?;
     }
 
-    //Querying the DB and printing to check
+    // Querying the DB and printing each line to check contents.
     for row in client.query("SELECT name FROM rangers", &[])? {
         let name: &str = row.get(0);
         println!("Found: {}", name);
     }
-    //Clearing the DB
+
+    // Dropping the table after were done executing. Comment out if needed.
     client.batch_execute(
         "
     DROP TABLE rangers;
     ",
     )?;
+
     Ok(())
 }
